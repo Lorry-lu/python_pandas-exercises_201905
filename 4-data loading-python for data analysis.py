b36b52bff6098b3e44360e2db93c31d49275c138
ShaoZC <854697580@qq.com>
2019-07-01 10:50:46 +0800

    Create 4-data loading-python for data analysis.py

---
+++ 4-data loading-python for data analysis.py
@@ -0,0 +1,12 @@
+#chapter 6
+#data loading, storage, and file formats
+
+#6.1 reading and writing data in text format
+import pandas as pd
+import numpy as np
+#!cat ex1.csv
+pf=pd.read_csv("C:/Users/shaoz/.PyCharmCE2019.1/config/scratches/ex1.csv")
+print('pf is',pf)
+
+
+
